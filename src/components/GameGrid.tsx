79d4fc20eac0a7a16e6b60e6d1c1fe0696f49938
MahmoudAleek <51204248+MahmoudAleek@users.noreply.github.com>
2024-03-04 18:09:19 +0200

    Handling no data

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,6 +14,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   if (error) return <Text> {error}</Text>;
+  if (data.length === 0) return <Text> No data</Text>;
 
   return (
     <SimpleGrid
