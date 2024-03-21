f1d7a93119a3c1e731d3b906cb282ac0c42c318b
MahmoudAleek <51204248+MahmoudAleek@users.noreply.github.com>
2024-03-21 18:07:37 +0200

    Improve usePlatforms hook

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import useData, { FetchResponse } from "./useData";
 import apiClient from "../services/api-client";
+import platforms from "../data/platforms";
 
 export interface Platform {
   id: number;
@@ -15,6 +16,8 @@ const usePlatforms = () =>
       apiClient
         .get<FetchResponse<Platform>>("/platforms/lists/parents")
         .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    initialData: { count: platforms.length, results: platforms },
   });
 
 export default usePlatforms;
